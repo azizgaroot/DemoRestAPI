c21f8c3e4c921d7a910cac0e0a4e765444acea70
azizgaroot <zee.garoot@gmail.com>
2024-09-15 13:47:05 +0300

    Lombok utilization #1

--- src/main/java/com/example/demoRestAPI/Service/UsersService.java
+++ src/main/java/com/example/demoRestAPI/Service/UsersService.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class UsersService {
 
-    List<Users> users = Arrays.asList(new Users("e20007699", "0537977218", "Al Jawhara", "Garoot"), new Users("e1", "xxxxx", "Al Shatti Dist", "Elm ") )
+    List<Users> users = Arrays.asList(new Users("e20007699", "0537977218", "Al Jawhara", "Garoot"), new Users("e1", "xxxxx", "Al Shatti Dist", "Elm ") );
 
     public List<Users> getUsers(){
-        return null
+        return null;
 
     }
 }
